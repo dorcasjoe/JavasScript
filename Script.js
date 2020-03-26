20bb5beaf521ed576d23acffa5b441fba0799a6e
Dorcas <44678748+dorcasjoe@users.noreply.github.com>
2020-03-26 11:34:19 +0100

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,10 @@
+let myName = 'Dorcas';
+let myCourses = ['html ', ' css ', ' JavasScript'];
+  console.log('My name is ' + myName +  ' and my courses are ' + myCourses);
+  
+  
+  for (let i= 1; i<=200; i++) {
+    if (i%2 !==0) {
+      console.log(i);
+    }
+  }
